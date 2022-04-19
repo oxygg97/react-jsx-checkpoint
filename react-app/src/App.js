23052bd87b5b2372179e89f2443180da8a76c5cf
chakabaka <ala.ben.cheikh@outlook.com>
2022-04-19 09:16:07 +0200

    first commit

---
+++ react-app/src/App.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import img1 from './images/download.jpg'
+import img2 from './images/kim2.jpg'
+
+export const App = () => {
+  return (
+       <div className='gg' >
+
+          <h1 class="title red"> ala ben cheikh</h1>
+            <br/>
+            <img src={img1} alt='comrades'/>
+            <br/>
+            <img src={img2} alt='comrades2'/>
+      
+      
+      
+<br/>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/inBGKQ84S5g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+</div>
+    
+    
+    
+  )
+}
+
+export default App;
